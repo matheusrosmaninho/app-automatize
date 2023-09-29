e799b252ca4ab7b765ba0e333e20dd323ec8b5f9
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-09-29 00:05:50 -0300

    fix: Ajuste no ec2

--- infra/homologation/terraform/ec2.tf
+++ infra/homologation/terraform/ec2.tf
@@ -4,30 +4,40 @@ resource "aws_key_pair" "infra_key_app" {
 }
 
 resource "aws_instance" "infra_ec2_app" {
-  ami = var.ec2Ami
-  instance_type = var.ec2InstanceType
-  subnet_id = aws_subnet.infra_subnet_app.id
+  ami                         = var.ec2Ami
+  instance_type               = var.ec2InstanceType
+  subnet_id                   = aws_subnet.infra_subnet_app.id
   associate_public_ip_address = true
-  key_name = aws_key_pair.infra_key_app.key_name
-  security_groups = [ aws_security_group.infra_sg_app.id ]
+  key_name                    = aws_key_pair.infra_key_app.key_name
+  security_groups             = [aws_security_group.infra_sg_app.id]
   root_block_device {
     volume_size = 30
     volume_type = "gp3"
+
+    tags = {
+      Name = "${var.appName}-${var.env}"
+      Type = "EBS"
+    }
   }
 
   disable_api_termination = false
-  disable_api_stop = false
+  disable_api_stop        = false
 
   depends_on = [
     aws_key_pair.infra_key_app,
     aws_subnet.infra_subnet_app,
     aws_security_group.infra_sg_app
-   ]
+  ]
+
+  tags = {
+    Name = "${var.appName}-${var.env}"
+    Type = "EC2"
+  }
 }
 
 resource "aws_eip" "infra_ec2_eip_app" {
   instance = aws_instance.infra_ec2_app.id
   domain   = "vpc"
 
-  depends_on = [ aws_instance.infra_ec2_app ]
+  depends_on = [aws_instance.infra_ec2_app]
 }
