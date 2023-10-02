9e801b1dc55249cb8ef8fd2f4d062ffb93012d25
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-10-02 19:39:42 -0300

    fix: Remove single nat gateway

--- infra/homologation/terraform/vpc.tf
+++ infra/homologation/terraform/vpc.tf
@@ -9,7 +9,6 @@ module "vpc" {
   public_subnets  = ["${var.prefixCidr}.101.0/24", "${var.prefixCidr}.102.0/24", "${var.prefixCidr}.103.0/24"]
 
   enable_nat_gateway = false
-  single_nat_gateway = false
   enable_dns_hostnames = true
   enable_dns_support = true
 
