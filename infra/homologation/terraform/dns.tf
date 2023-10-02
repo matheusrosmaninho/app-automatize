f719977dc8435316f4785a4b06998100d3bd2ee1
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-10-02 19:44:53 -0300

    fix: Comentario no dns

--- infra/homologation/terraform/dns.tf
+++ infra/homologation/terraform/dns.tf
@@ -4,6 +4,7 @@ resource "cloudflare_record" "infra_ec2_app_domain" {
   value   = aws_eip.infra_ec2_eip_app.public_ip
   type    = "A"
   proxied = true
+  comment = "${var.appName}-${var.productName}-${var.env}"
 
   depends_on = [
     aws_eip.infra_ec2_eip_app,
